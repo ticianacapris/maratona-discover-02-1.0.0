03fb2d4d72eb9cde36436eaf6fc81a4f8c025d8a
tcapris <61629953+tcapris@users.noreply.github.com>
2021-04-02 16:42:34 +0100

    Ejs refactoring

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require ("express")
 const server = express()
 const routes = require ("./routes")
 
+server.set('view engine','ejs')
+
 //habilitar arquivos statics
 server.use(express.static('public'))
 
